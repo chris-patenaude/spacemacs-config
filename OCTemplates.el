1bc9263a014f02748cce1ff21a06b5d8fd83077f
Patenaude, Christopher T <patenauc@oregonstate.edu>
2019-05-02 16:36:16 -0700

    added some documentation

--- OCTemplates.el
+++ OCTemplates.el
@@ -1,40 +1,39 @@
-  ;; ---------------------
-  ;; org-capture templates
-  ;; ---------------------
-  (setq org-capture-templates
-        (quote ()))
+;; Create the template list
+(setq org-capture-templates
+      (quote ()))
 
-  (add-to-list 'org-capture-templates
-               '("t" "Task"))
+;; Setup task multi-key prefix
+(add-to-list 'org-capture-templates
+             '("t" "Task"))
 
-  ;; Work TODO template (Prompts for Tag)
-  (add-to-list 'org-capture-templates
-               '("tw" "Work-related Task"  entry
-                 (file+headline "~/Dropbox/Notes/work.org" "Tasks")
-                 "* TODO %?\t%^g\n%U\n%a\n"))
+;; Work TODO template (Prompts for Tag)
+(add-to-list 'org-capture-templates
+             '("tw" "Work-related Task"  entry
+               (file+headline "~/Dropbox/Notes/work.org" "Tasks")
+               "* TODO %?\t%^g\n%U\n%a\n"))
 
-  ;; Personal TODO template (Prompts for Tag)
-  (add-to-list 'org-capture-templates
-               '("tt" "todo" entry
-                 (file+headline org-default-notes-file "Tasks")
-                 "* TODO %?\t%^g\n%U\n%a\n"))
+;; Personal TODO template (Prompts for Tag)
+(add-to-list 'org-capture-templates
+             '("tt" "todo" entry
+               (file+headline org-default-notes-file "Tasks")
+               "* TODO %?\t%^g\n%U\n%a\n"))
 
-  ;; Idea template
-  (add-to-list 'org-capture-templates
-               '("i" "idea" entry
-                 (file+headline org-default-notes-file "Ideas")
-                 "* %? :IDEA:\n%U\n%a\n"))
+;; Idea template
+(add-to-list 'org-capture-templates
+             '("i" "idea" entry
+               (file+headline org-default-notes-file "Ideas")
+               "* %? :IDEA:\n%U\n%a\n"))
 
-  ;; Note template
-  (add-to-list 'org-capture-templates
-               '("n" "note" entry
-                 (file+headline org-default-notes-file "Notes")
-                 "* %? :NOTE:\n%U\n%a\n"))
+;; Note template
+(add-to-list 'org-capture-templates
+             '("n" "note" entry
+               (file+headline org-default-notes-file "Notes")
+               "* %? :NOTE:\n%U\n%a\n"))
 
-  ;; Code Snippet template (Prompts for Tag)
-  (add-to-list 'org-capture-templates
-               '("c" "Code Snippet" entry
-                 (file+headline org-default-notes-file "Code Snippets")
-                 "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC"))
+;; Code Snippet template (Prompts for Tag)
+(add-to-list 'org-capture-templates
+             '("c" "Code Snippet" entry
+               (file+headline org-default-notes-file "Code Snippets")
+               "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC"))
 
 (provide 'OCTemplates)
